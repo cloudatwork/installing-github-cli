e6feaf6739af780fd248fb55c63bb85f9b7c0ea2
cloudatwork <143493780+cloudatwork@users.noreply.github.com>
2023-11-23 04:44:45 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,3 +12,15 @@ sudo apt install gh
 
 # Executing this on your Ubuntu
 # bash <(curl https://raw.githubusercontent.com/cloudatwork/installing-github-cli/main/install.sh)
+
+# Loginto Github
+# gh auth login
+
+# ? What account do you want to log into? GitHub.com
+? What is your preferred protocol for Git operations? HTTPS
+? Authenticate Git with your GitHub credentials? No
+? How would you like to authenticate GitHub CLI?  [Use arrows to move, type to filter]
+  Login with a web browser
+> Paste an authentication token
+
+# You have to create a Github Token here: 
