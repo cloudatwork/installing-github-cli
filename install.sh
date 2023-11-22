c0751e90a4088099c9ec9f38b29985d007a0f100
cloudatwork <143493780+cloudatwork@users.noreply.github.com>
2023-11-23 04:51:06 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-# https://github.com/cli/cli/blob/trunk/docs/install_linux.md
+# CODE REFERENCE: https://github.com/cli/cli/blob/trunk/docs/install_linux.md
 
 type -p curl >/dev/null || (sudo apt update && sudo apt install curl -y)
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
@@ -10,17 +10,21 @@ curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo 
 sudo apt update
 sudo apt install gh
 
-# Executing this on your Ubuntu
+# INSTALLING ON YOUR UBUNTU
 # bash <(curl https://raw.githubusercontent.com/cloudatwork/installing-github-cli/main/install.sh)
 
-# Loginto Github
+# CREATE A GITHUB TOKEN HERE AND NOTE IN IN A NOTEPAD: https://github.com/settings/tokens
+
+# LOGIN TO GITHUB ON YOUR UBUNTU (FOLLOW ONSCREEN INSTRUCTIONS)
 # gh auth login
 
 # ? What account do you want to log into? GitHub.com
-? What is your preferred protocol for Git operations? HTTPS
-? Authenticate Git with your GitHub credentials? No
-? How would you like to authenticate GitHub CLI?  [Use arrows to move, type to filter]
-  Login with a web browser
-> Paste an authentication token
+# ? What is your preferred protocol for Git operations? HTTPS
+# ? Authenticate Git with your GitHub credentials? No
+# ? How would you like to authenticate GitHub CLI?  [Use arrows to move, type to filter]
+# Login with a web browser
+# > Paste an authentication token {PASTE THE TOKEN YOU SAVED ABOVE}
 
-# You have to create a Github Token here: 
+# NOW YOU ARE AUTHENTICATED TO GITHUB
+
+
